59341ebb960ed47475681261861da70920275e19
Lucid <70743392+lucidbit@users.noreply.github.com>
2020-10-06 15:03:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ cvs.width = window.innerWidth;
 cvs.height = window.innerHeight;
 
 const game = new Game(ctx, cvs.width, cvs.height, []);
-game.addGameObject(new GameRectangle(20, 20, 40, 40, { x: 1, y: 0 }));
 
 setInterval(() => { game.update(); }, 10);
