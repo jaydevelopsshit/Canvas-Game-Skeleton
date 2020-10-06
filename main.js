aa106a13f27586ede3b4f5da72119f4da76a410e
Lucid <70743392+lucidbit@users.noreply.github.com>
2020-10-06 15:04:53 -0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const cvs = document.getElementById("canvas");
-const ctx = cvs.getContext("2d");
-cvs.width = window.innerWidth;
-cvs.height = window.innerHeight;
-
-const game = new Game(ctx, cvs.width, cvs.height, []);
-
-setInterval(() => { game.update(); }, 10);
